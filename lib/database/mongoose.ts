0d7728646c6bd73e070af2809de53ebd29ac523d
Alivin Shiva <alivin612003@gmail.com>
2024-07-14 02:39:25 +0530

    bug fix

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -15,15 +15,21 @@ if (!cached) {
 
 export const connectToDatabase = async () => {
     if (cached.conn) {
-        return cached.conn
+        console.log("Using cached database connection");
+        return cached.conn;
     }
 
     if (!MONGODB_URL) {
-        throw new Error('MONGODB_URL is not set in the environment variables')
+        throw new Error('MONGODB_URL is not set in the environment variables');
     }
 
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URL, { dbName: 'ArtizenAI', bufferCommands: false })
-
-    cached.conn = await cached.promise
-    return cached.conn
+    try {
+        cached.promise = cached.promise || mongoose.connect(MONGODB_URL, { dbName: 'artizenai', bufferCommands: false });
+        cached.conn = await cached.promise;
+        console.log("Database connected successfully");
+        return cached.conn;
+    } catch (error) {
+        console.error("Database connection failed", error);
+        throw new Error("Database connection failed");
+    }
 }
--- middleware.ts
+++ middleware.ts
@@ -8,12 +8,30 @@
 //   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 // };
 
-import { authMiddleware } from "@clerk/nextjs/server";
+// import { authMiddleware } from "@clerk/nextjs/server";
  
-export default authMiddleware({
-  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
+// export default authMiddleware({
+//   publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
+// });
+ 
+// export const config = {
+//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api/webhooks(.*)",
+]);
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect();
+  }
 });
- 
+
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
